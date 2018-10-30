10d3b75388fc0d2a255141b89f337aa2fee7fd9f
matt fyke <mfyke@live.com>
2018-10-30 16:16:53 -0700

    display and timer functions done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,29 +17,83 @@ var question8 = new question("What is the name of the evil midget?", "prince cha
 var question9 = new question("What is the name of the evil midget?", "prince charming", "lord farquaad", "baby legs", "donkey", "lord farquaad");
 var question10 = new question("What is the name of the evil midget?", "prince charming", "lord farquaad", "baby legs", "donkey", "lord farquaad");
 var questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
-var activeQuestion = questions[0];
+var questionNumber = 1;
+var activeQuestion = questions[questionNumber-1];
 var correctQuestions = 0;
+var timer = 20;
+var countdown;
 
 var startGame = function() {
-	activeQuestion=questions[0];
+	timer=20;
+	questionNumber = 1;
+	activeQuestion=questions[questionNumber-1];
 	correctQuestions=0;
 	$("#questionArea").hide();
 	$(".answers").hide();
+	$("#timerArea").html("Answer as many Shrek trivia questions as you can! There is no penalty for incorrect answers! You will have 20 seconds per question. Press space bar to begin!");
 }
 
 $(document).ready(function() {
 	startGame();
+	// displayQuestion();
 });
+
+window.onkeyup = function() {
+	console.log(event.code);
+	if (event.code == "Space" && activeQuestion == questions[0] && timer > 19) {
+		displayQuestion();
+		displayTimer();
+		startTimer();
+	}
+}
 
 // when space bar is pressed game starts (first question is displayed, answers displayed, instructions turn into a timer)
 
+//display current question and possible answers
+var displayTimer = function() {
+	$("#timerArea").html(timer);
+}
+
+var displayQuestion = function () {
+	$("#questionArea").show().html(activeQuestion.text);
+	$(".answers").show();
+	$("#answerA").html(activeQuestion.ansA);
+	$("#answerB").html(activeQuestion.ansB);
+	$("#answerC").html(activeQuestion.ansC);
+	$("#answerD").html(activeQuestion.ansD);
+}
 
 // timer counts down from 15 seconds until an answer is chosen or time tuns out
+var startTimer = function() {
+	countdown = setInterval(function(){
+		timer--;
+		displayTimer();
+		if(timer==0) {
+			questionWrong();
+		}
+	},1000);
+}
 
 // answer correct (correct answers goes up, next question is displayed, timer reset )
-
+var answerCorrect = function() {
+	correctQuestions++;
+	questionWrong();
+}
 // answer incorrect or not answered (next question is displayed, timer reset);
-
+var questionWrong = function() {
+	clearInterval(countdown);
+	questionNumber++;
+	if(questionNumber>questions.length){
+		endGame();
+	}
+	else{
+		activeQuestion=questions[questionNumber-1];
+		timer=20;
+		displayQuestion();
+		displayTimer();
+		startTimer();
+	}
+}
 // at the end of the questions number of questions answered correctly is displayed
 
 // press button to return to the beginning of the quiz
